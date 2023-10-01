40b9bd8eea60e0e7a07f7e98d03348c60e8a8ee7
Viktor Sokolov <viktorsokolov.and@gmail.com>
2023-10-01 13:51:11 +0300

    init project

--- next.config.js
+++ next.config.js
@@ -1,4 +1,36 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+/* eslint-disable import/no-extraneous-dependencies */
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true'
+});
 
-module.exports = nextConfig
+const nextConfig = {
+    eslint: {
+        dirs: ['./src', './lib']
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**',
+                port: '',
+                pathname: '**'
+            }
+        ]
+    },
+    poweredByHeader: false,
+    // compress: false,
+    basePath: '',
+    // The starter code load resources from `public` folder with `router.basePath` in React components.
+    // So, the source code is "basePath-ready".
+    // You can remove `basePath` if you don't need it.
+    reactStrictMode: true,
+    output: 'standalone',
+};
+
+module.exports = () => {
+    const plugins = [withBundleAnalyzer];
+    return plugins.reduce((acc, next) => next(acc), nextConfig);
+};
